d6cb4c5441fae3fb2db2f9097ffdcc0c1c1466c3
Michael R.F <mrflowers22@uwalumni.com>
2019-09-30 10:59:52 -0600

    added a view controller to the storyboard and attached the iboutlets and  inactions

---
+++ DeckOfOneCardGP/CardViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CardViewController.swift
+//  DeckOfOneCardGP
+//
+//  Created by Michael Flowers on 9/30/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var drawCardProperties: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func drawButtonPressed(_ sender: UIButton) {
+    }
+    
+}
